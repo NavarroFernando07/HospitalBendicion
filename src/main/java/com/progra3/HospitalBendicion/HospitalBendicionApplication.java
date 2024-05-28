ab86d3a28f0e7f4cdbfaf29e91d45c876958ea0c
luis <lnavarrog2@miumg.edu.gt>
2024-05-28 17:46:08 -0600

    Se agrega proyecto

---
+++ src/main/java/com/progra3/HospitalBendicion/HospitalBendicionApplication.java
@@ -0,0 +1,13 @@
+package com.progra3.HospitalBendicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBendicionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalBendicionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progra3/HospitalBendicion/HospitalBendicionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra3.HospitalBendicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalBendicionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
